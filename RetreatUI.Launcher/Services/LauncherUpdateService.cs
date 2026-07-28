82834a04336ff2c744fb804194637336e7b31ee2
Retreat <gaarn94@gmail.com>
2026-07-29 00:21:06 +0200

    Update launcher fallback version to 0.2.1

--- RetreatUI.Launcher/Services/LauncherUpdateService.cs
+++ RetreatUI.Launcher/Services/LauncherUpdateService.cs
@@ -22,7 +22,7 @@ public sealed class LauncherUpdateService
     }
 
     public string CurrentVersion =>
-        Assembly.GetExecutingAssembly().GetName().Version?.ToString(3) ?? "0.2.0";
+        Assembly.GetExecutingAssembly().GetName().Version?.ToString(3) ?? "0.2.1";
 
     public async Task<LauncherUpdate?> CheckForUpdateAsync(CancellationToken cancellationToken = default)
     {
