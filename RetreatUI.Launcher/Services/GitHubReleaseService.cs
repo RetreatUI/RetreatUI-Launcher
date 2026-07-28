d821b9616d4a4a3afd0f294eba139698a590474c
Retreat <gaarn94@gmail.com>
2026-07-29 00:18:48 +0200

    Update launcher user agent to 0.2.1

--- RetreatUI.Launcher/Services/GitHubReleaseService.cs
+++ RetreatUI.Launcher/Services/GitHubReleaseService.cs
@@ -22,7 +22,7 @@ public sealed class GitHubReleaseService
             Timeout = TimeSpan.FromSeconds(30)
         };
         _httpClient.DefaultRequestHeaders.UserAgent.Add(
-            new ProductInfoHeaderValue("RetreatUI-Launcher", "0.1.0"));
+            new ProductInfoHeaderValue("RetreatUI-Launcher", "0.2.1"));
         _httpClient.DefaultRequestHeaders.Accept.Add(
             new MediaTypeWithQualityHeaderValue("application/vnd.github+json"));
     }
