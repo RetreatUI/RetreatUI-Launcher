42981cb39d2670d03d035008f24f7bc73d7c452e
Retreat <gaarn94@gmail.com>
2026-07-28 19:49:19 +0200

    Fix missing HTTP namespace in launcher build

--- RetreatUI.Launcher/Services/GitHubReleaseService.cs
+++ RetreatUI.Launcher/Services/GitHubReleaseService.cs
@@ -1,5 +1,11 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
 using RetreatUI.Launcher.Models;
 
 namespace RetreatUI.Launcher.Services;
