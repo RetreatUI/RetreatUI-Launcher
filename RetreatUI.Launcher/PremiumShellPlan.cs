80422d64e1fca36757159709d48d695fae7df1f0
Retreat <gaarn94@gmail.com>
2026-08-07 00:45:30 +0200

    Add premium shell migration map

---
+++ RetreatUI.Launcher/PremiumShellPlan.cs
@@ -0,0 +1,27 @@
+namespace RetreatUI.Launcher;
+
+internal static class PremiumShellPlan
+{
+    public static readonly string[] PreservedControlNames =
+    {
+        "LauncherVersionText",
+        "StableRadio",
+        "BetaRadio",
+        "CoARadio",
+        "TbcRadio",
+        "LauncherUpdateButton",
+        "InstalledVersionText",
+        "LatestVersionText",
+        "PathText",
+        "DownloadProgress",
+        "UpdateButton",
+        "RefreshButton",
+        "ReleaseNotesText",
+        "StatusDot",
+        "StatusText",
+        "OpenFolderButton",
+        "LaunchButton",
+    };
+
+    public const string DesignTarget = "Premium dark-fantasy shell with native DPI-safe controls";
+}
